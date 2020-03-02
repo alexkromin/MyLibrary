23c92523b42d7e7ad6f92350a84622974dd77539
alexkromin <45353271+alexkromin@users.noreply.github.com>
2020-03-02 21:59:38 +0300

    EnumerableExtension
    Add 2 optimized methods for IEnumerable: ToArray and ToList.

---
+++ EnumerableExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLibrary
+{
+    public static class EnumerableExtension
+    {
+        public static TSource[] ToArray<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (count < 0) 
+                throw new ArgumentOutOfRangeException("count");
+            var array = new TSource[count];
+            int i = 0;
+            foreach (var item in source)
+            {
+                array[i++] = item;
+            }
+            if (i != count)
+                throw new ArgumentOutOfRangeException();
+            return array;
+        }
+
+        public static List<TSource> ToList<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+            var list = new List<TSource>(count);
+            foreach (var item in source)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
+    }
+}
